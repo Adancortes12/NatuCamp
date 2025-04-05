bcbfb533aa5bc4b3afa44fb7e4b8340381b681b4
Adancortes12 <cortesadan1234@gmail.com>
2025-04-05 12:55:59 -0600

    solicitud

--- natucamp/server/server.js
+++ natucamp/server/server.js
@@ -9,6 +9,26 @@ const db= mysql.createConnection({
     database:'NatuCamp'
 });
 
+app.post('/create',(req,res)=>{
+    const idUsuario= req.body.idUsuario;
+    const nombre= req.body.nombre;
+    const primerAp= req.body.apellido;
+    const segundoAp= req.body.segundoApellido;
+    const fechaNac= req.body.fechaNacimiento;
+    const correo= req.body.correo;
+    const celular= req.body.celular;
+    const usuario= req.body.usuario;
+    const contraseña= req.body.contraseña;
+    db.query('INSERT INTO usuario (idUsuario,nombre,primerAp,segundoAp,fechaNac,correo,celular,usuario,contraseña) VALUES (?,?,?,?,?,?,?,?,?)',
+    [idUsuario,nombre,primerAp,segundoAp,fechaNac,correo,celular,usuario,contraseña], (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send('Usuario creado');
+        }
+    });
+})
+
 app.listen(3001,()=>{
     console.log('corriendo loco en 3000')
 }); 
