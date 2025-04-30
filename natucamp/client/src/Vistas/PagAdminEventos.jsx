9d50204fcac5f7d0d506237d3d58aab34c3ee969
Marianuchis <mariana.anguiano@hotmail.com>
2025-04-30 09:12:40 -0600

    30/04/25
    Pagina de administracion  de eventos pt2

--- natucamp/client/src/Vistas/PagAdminEventos.jsx
+++ natucamp/client/src/Vistas/PagAdminEventos.jsx
@@ -3,9 +3,37 @@ export function PagAdminEventos() {
   return (
     <>
       <h1>Administrar evento</h1>
-      <div className={"contenedor"}>
-        <p>Aca va todo lo demas </p>
-      </div>
+      <main>
+        <div className="contenedor">
+          <div className="caja">
+            <div className="formulario">
+              <input placeholder="Nombre" id="campoNombre"></input>
+              <input placeholder="Descripción" id="campoDesc"></input>
+              <select class="form-select" id="campoTipo">
+                <option selected>Tipo</option>
+                <option value="1">Educativo</option>
+                <option value="2">Entretenimiento</option>
+                <option value="3">Deportivo</option>
+              </select>
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text">$</span>
+                </div>
+                <input type="text" class="form-control" aria-label="precio" />
+                <div class="input-group-append">
+                  <span class="input-group-text">.00</span>
+                </div>
+              </div>
+              <button id="botonGuardar">Guardar</button>
+              <button id="botonCancelar">Cancelar</button>
+            </div>
+            <div id="imgDisplay"></div>
+            <div id="insertarImagen">
+              <button>Agregar imagen</button>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
