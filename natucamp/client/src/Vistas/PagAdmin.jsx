e663c77bf419f09f5de9962fc9041ce42c8498e2
Marianuchis <mariana.anguiano@hotmail.com>
2025-05-06 12:34:48 -0600

    06/05/25
    Pagina de administrador pt1

--- natucamp/client/src/Vistas/PagAdmin.jsx
+++ natucamp/client/src/Vistas/PagAdmin.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import styles from "./StylesAdmin.module.css";
+
 export function PagAdmin() {
-  <div>
-    <h1>Aca va el desmadre este alv</h1>
-  </div>;
+  const ingresarLink = () => {
+    prompt("Ingrese el link de la imagen");
+  };
+  return (
+    <>
+      <div className={styles.contenedor}>
+        <div className={styles.card}>
+          <p className={styles.cardTitulo}>EVENTOS</p>
+          <div className={styles.contenedorIcono}>
+            <i className={`bi bi-calendar-date ${styles.icono}`}></i>
+          </div>
+          <Link to="/AdminEventos" className={styles.botonAdmin}>
+            Administrar eventos
+          </Link>
+        </div>
+        <div className={styles.card}>
+          <p className={styles.cardTitulo}>ESPECIES</p>
+          <div className={styles.contenedorIcono}>
+            <i className={`bi bi-journal-text ${styles.icono}`}></i>
+          </div>
+          <Link to="/AdminEspecies" className={styles.botonAdmin}>
+            Administrar especies
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
